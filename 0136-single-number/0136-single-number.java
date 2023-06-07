aa2b727adbd5987016ae0905ae979ae1ffc60e7d
holland-cw3 <101285025+holland-cw3@users.noreply.github.com>
2023-06-07 15:37:14 -0400

    Time: 7 ms (32.95%), Space: 45.1 MB (5.67%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> frequency = new HashMap<>();
-
-        for(int num : nums){
-            frequency.put(num, frequency.containsKey(num) ? 2 : 1);
+        Arrays.sort(nums);
+        if(nums.length == 1 ||nums[0] != nums[1]){
+            return nums[0];
         }
-
-        for(int key : frequency.keySet()){
-            if(frequency.get(key) == 1){
-                return key;
+       
+        for(int i = 1; i < nums.length - 1; i++){
+            if(nums[i] != nums[i + 1] && nums[i] != nums[i - 1]){
+                return nums[i];
             }
         }
+        return nums[nums.length -1];
+        
 
-        return 0;
+
+
+
+
     }
 }
