aa2037b33ab97669b4539bd16b48fac49cc738ec
holland-cw3 <101285025+holland-cw3@users.noreply.github.com>
2023-06-07 15:44:10 -0400

    Time: 13 ms (25.1%), Space: 44 MB (30.56%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,21 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        if(nums.length == 1 ||nums[0] != nums[1]){
-            return nums[0];
-        }
-       
-        for(int i = 1; i < nums.length - 1; i++){
-            if(nums[i] != nums[i + 1] && nums[i] != nums[i - 1]){
-                return nums[i];
+        HashSet<Integer> unique = new HashSet<>();
+
+        for(int num : nums){
+            if(unique.contains(num)){
+                unique.remove(num);
+            }
+            else{
+                unique.add(num);
             }
         }
-        return nums[nums.length -1];
-        
-
-
-
-
+        return unique.iterator().next();
 
     }
 }
